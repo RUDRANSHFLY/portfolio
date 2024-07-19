72e8053f5911d546ea222597f7d41623a8a53a66
RUDRANSHFLY <mayamayavir@gmail.com>
2024-07-19 16:24:10 +0530

    Sanity Config  Client Side  Add

---
+++ sanity.ts
@@ -0,0 +1,17 @@
+import { createClient } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
+const config = {
+  baseUrl: process.env.NEXT_PUBLIC_SANITY_BASE_URL,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2021-03-25",
+  useCdn: process.env.NODE_ENV === "production",
+};
+
+const sanityClient = createClient(config);
+
+const urlFor = (source: any) => {
+  imageUrlBuilder(sanityClient).image(source);
+};
+
+export { urlFor, sanityClient };
