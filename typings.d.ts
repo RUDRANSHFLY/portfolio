7f33ffff15b05ed1309edadd86b22116d1292d3a
RUDRANSHFLY <mayamayavir@gmail.com>
2024-07-19 16:24:33 +0530

    Neccsity Typing  Add

---
+++ typings.d.ts
@@ -0,0 +1,73 @@
+interface SanityBody {
+  _createdAt: string;
+  _id: string;
+  _rev: string;
+  _updatedAt: string;
+}
+
+interface Image {
+  _type: "image";
+  assest: {
+    _ref: string;
+    _type: "reference";
+  };
+}
+
+interface PageInfo extends SanityBody {
+  _type: "pageInfo";
+  address: string;
+  backgroundInformation: string;
+  email: string;
+  role: string;
+  heroImage: string;
+  phoneNumber: string;
+  profilePic: string;
+}
+
+interface Technology extends SanityBody {
+  _type: "skill";
+  image: Image;
+  progss: number;
+  title: string;
+}
+
+interface Skill extends SanityBody {
+  _type: "skill";
+  image: Image;
+  progress: number;
+  title: string;
+}
+
+interface Project extends SanityBody {
+  title: string;
+  _type: "project";
+  image: Image;
+  linktoBuild: string;
+  summary: string;
+  technologies: Technology[];
+}
+
+interface Education extends SanityBody {
+  _type: "eduation";
+  educationImage: Image;
+  instituteTitle: string;
+  educationTitle: string;
+  isCurrentlyEducationHere: boolean;
+}
+
+interface Social extends SanityBody {
+  _type: "social";
+  title: string;
+  url: string;
+}
+
+export {
+  SanityBody,
+  Social,
+  Skill,
+  Education,
+  Image,
+  PageInfo,
+  Technology,
+  Project,
+};
