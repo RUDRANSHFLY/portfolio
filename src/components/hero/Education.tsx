7c53d3afffb4956541baa60b0b2e6c70b5e83067
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-06 16:06:42 +0530

    Spcae Change

--- src/components/hero/Education.tsx
+++ src/components/hero/Education.tsx
@@ -11,20 +11,21 @@ type Props = {
 
 const HellaEducation = ({ education }: Props) => {
   return (
-    <MaxWidthWrapper>
+    <div
+      className={
+        "h-screen w-screen relative flex flex-col justify-center items-center text-left md:flex-row   mx-auto"
+      }
+    >
+      <h3 className={"heroTitle"}>Education</h3>
       <div
-        className={
-          "h-screen relative flex flex-col justify-evenly items-center text-left md:flex-row max-w-full px-10 mx-auto"
-        }
+        className="w-screen
+         p-10 flex sm:justify-center space-x-5 snap-x snap-mandatory overflow-x-scroll overflow-y-hidden  scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-red-500 "
       >
-        <h3 className={"heroTitle"}>Education</h3>
-        <div className="absolute top-24 w-full p-10 flex justify-center space-x-5 snap-x snap-mandatory overflow-x-scroll overflow-y-hidden  scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-red-500 ">
-          {education.map((education) => (
-            <EducationCard education={education} key={education._id} />
-          ))}
-        </div>
+        {education.map((education) => (
+          <EducationCard education={education} key={education._id} />
+        ))}
       </div>
-    </MaxWidthWrapper>
+    </div>
   );
 };
 
