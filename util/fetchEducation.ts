cd45430639aebf7e601711b7cbcbbdf564d2af88
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-06 16:12:30 +0530

    Server Action to fetch Education

--- util/fetchEducation.ts
+++ util/fetchEducation.ts
@@ -1,13 +1,13 @@
 import { groq } from "next-sanity";
 import { Education } from "../typings";
-import { sanityClient } from "../sanity";
+import { sanityFetch } from "../sanity";
 
 const fetchEducation = async () => {
   const query = groq`
   *[_type == "education"]
 `;
 
-  const education: Education[] = await sanityClient.fetch(query);
+  const education: Education[] = await sanityFetch({ query });
 
   return education;
 };
