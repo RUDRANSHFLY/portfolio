14b677e62acc8ee150d0867441c180aab611c478
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-06 16:07:32 +0530

    Dynamic Project array add

--- src/components/hero/Projects.tsx
+++ src/components/hero/Projects.tsx
@@ -4,10 +4,14 @@ import MaxWidthWrapper from "@/helper/ui/MaxWidthWrapper";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import React from "react";
+import { Project } from "../../../typings";
+import ProjectCard from "../cards/ProjectCard";
 
-const Projects = () => {
-  const projects = [1, 2, 3, 4, 5];
+type Props = {
+  projects: Project[];
+};
 
+const Projects = ({ projects }: Props) => {
   return (
     <MaxWidthWrapper className={"max-w-full"}>
       <div
@@ -23,43 +27,7 @@ const Projects = () => {
           }
         >
           {projects.map((project, i) => (
-            <div
-              key={i}
-              className="mt-20 mb-10 w-screen h-screen flex-shrink-0 snap-center flex flex-col space-y-10 items-center justify-center p-10"
-            >
-              <motion.div
-                initial={{
-                  opacity: 0,
-                  y: -300,
-                }}
-                transition={{
-                  duration: 1.2,
-                }}
-                whileInView={{ opacity: 1, y: 0 }}
-              >
-                <Image
-                  src={
-                    "https://st.depositphotos.com/1006269/2498/i/450/depositphotos_24980171-stock-photo-computer-laptop-mobile-phone-and.jpg"
-                  }
-                  height={400}
-                  width={400}
-                  alt={"project"}
-                />
-              </motion.div>
-              <div className={"max-w-6xl space-y-5"}>
-                <h4 className="sm:text-4xl font-semibold text-center underline decoration-red-400">
-                  Case Study of {i + 1} of {projects.length} : UPS Clone
-                </h4>
-                <p className="text-center text-xs sm:text-lg md:text-left">
-                  Netflix 2.0 app that use a Login and Logout Authentication
-                  with Gooogle . it had a beautiful Home Screen with all the
-                  movies looking just like Netflix . there is also a
-                  subscription page where you can see your action monthly
-                  subscription and we also use Stripe Payments for the monthly
-                  Netflix Subscription
-                </p>
-              </div>
-            </div>
+            <ProjectCard project={project} key={i} />
           ))}
         </div>
 
