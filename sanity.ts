d794379cf189308a25aca5056d1774af537e3165
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-06 15:55:56 +0530

    Revalidatte Add

--- sanity.ts
+++ sanity.ts
@@ -1,12 +1,31 @@
-import { createClient } from "next-sanity";
+import { createClient, type QueryParams } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 const config = {
   baseUrl: process.env.NEXT_PUBLIC_SANITY_BASE_URL,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2021-03-25",
-  useCdn: process.env.NODE_ENV === "production",
+  useCdn: true,
 };
+
+export async function sanityFetch<QueryResponse>({
+  query,
+  params = {},
+  revalidate = 5, // default revalidation time in seconds
+  tags = [],
+}: {
+  query: string;
+  params?: QueryParams;
+  revalidate?: number | false;
+  tags?: string[];
+}) {
+  return sanityClient.fetch<QueryResponse>(query, params, {
+    next: {
+      revalidate: tags.length ? false : revalidate, // for simple, time-based revalidation
+      tags, // for tag-based revalidation
+    },
+  });
+}
 
 const sanityClient = createClient(config);
 
